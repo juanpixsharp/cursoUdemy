381db49e26258b1cfea68fe93f4880281f87b717
Juan Martinez <juanpixsharp@gmail.com>
2023-04-09 21:14:02 -0600

    persona.model: Creacion de clase persona con 2 atributos

---
+++ src/app/persona.model.ts
@@ -0,0 +1,16 @@
+export class PErsona{
+    
+    nombre: string = '';
+    apellido: string = '';
+
+    constructor(nombre: string, apellido: string){
+        this.nombre=nombre;
+        this.apellido = apellido;
+    }
+
+
+    /*
+    SINTAXIS SIMPLIFICADA PARA CREAR ATRIBUTOS
+    constructor(public nombre:string, public apellido:string){}
+    */
+}
