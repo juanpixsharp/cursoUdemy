1838955334d530b482f56c34067bf04d90880d43
Juan Martinez <juanpixsharp@gmail.com>
2023-04-15 22:23:13 -0600

    Finalizamos Routing con ErrorComponent para todas las rutas no existentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PersonasComponent } from './personas/personas.component';
 import { FormularioComponent } from './personas/formulario/formulario.component';
+import { ErrorComponent } from './error/error.component';
 
 const routes: Routes = [
   {path: '', component: PersonasComponent},
@@ -9,7 +10,8 @@ const routes: Routes = [
   children:[
     {path:'agregar', component: FormularioComponent},
     {path:':id', component: FormularioComponent}
-  ]}
+  ]},
+  {path: '**', component: ErrorComponent},
 ]
 
 @NgModule({
